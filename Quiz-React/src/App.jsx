2d3aa36a3e90dbef2e4be7771a758e3b7cb43d2b
swap-git03 <swapnilfulpagare061003@gmail.com>
2025-02-01 19:13:26 +0530

    Initial commit

---
+++ Quiz-React/src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import Quiz from './components/Quiz'
+
+const App = () => {
+  return (
+    <>
+      <Quiz/>
+    </>
+  )
+}
+
+export default App
